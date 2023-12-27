352127b5de20a1d2f5558268f630383349748a22
LILANGYU <1577958961@qq.com>
2023-12-27 16:20:13 +0800

    add_oop

--- main.ts
+++ main.ts
@@ -855,6 +855,14 @@ namespace makerbit {
     background.schedule(notifyIrEvents, background.Thread.Priority, background.Mode.Repeat, REPEAT_TIMEOUT_MS);
   }
 
+  //% blockId="SET_IR_Key_Value"
+  //% block="SET_IR_Key_Value|Button %Button|key_value %Value"
+  //% Value.min = 0 Value.max = 255
+  //% weight=90
+  export function SET_IRKey_Value(Button:IrButton,Value:number): void {
+    Button = Value
+  }
+
   function notifyIrEvents() {
     if (irState.activeCommand === -1) {
       // skip to save CPU cylces
